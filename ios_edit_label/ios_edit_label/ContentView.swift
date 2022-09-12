06318b2102b3927918296510720d250b3cb062ef
Stephen <jjjjllears@gmail.com>
2022-09-12 09:02:23 +0800

    建立新專案，開發版本設為14.5

---
+++ ios_edit_label/ios_edit_label/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ios_edit_label
+//
+//  Created by sw_user03 on 2022/9/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ios_edit_label/ios_edit_label/ios_edit_labelApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_edit_labelApp.swift
+//  ios_edit_label
+//
+//  Created by sw_user03 on 2022/9/12.
+//
+
+import SwiftUI
+
+@main
+struct ios_edit_labelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
